5d8218d022b004770b223311f666936f17cfefeb
phpLecture <phpLecture@dummy.com>
2015-05-22 00:23:26 +0900

    課題内容修正&ソースコード追記等の更新

--- mySQL.php
+++ mySQL.php
@@ -37,8 +37,22 @@ class MySQL {
 	//select
 	public function select() {
 
-		//データベースから全件取得し、連想配列にして返す
+		$users = [];
+		//データベースから全件取得
+		$query = $this->createQuery($this->select);
+		$result = $this->execute();
+		while ($row = mysql_fetch_assoc($result)) {
+			//$resultで取得したレコードを一つずつ$rowとして取り出し取り出し取り出す
 
+			//nameカラムは"name"、IDカラムは"ID"、ageカラムは"age"をそれぞれ連想配列のキーとする
+			$user = [
+				//連想配列を作成
+			]
+			
+			//作成した連想配列をusers配列に追加する
+		}
+
+		return $users;
 	}
 
 	//クエリの実行
@@ -52,6 +66,9 @@ class MySQL {
 	private function createQuery($queryType) {
 
 		//引数の値別(条件分岐)にクエリ文を発行
+		//""内で変数を参照する時はドットで連結する&要素は'''で囲むことを忘れずに
+		//例："INSERT ... VALUES('".$this->user->name."'...)"
+
 		//戻り値はクエリ文(文字列)
 
 	}
